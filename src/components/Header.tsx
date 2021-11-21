e91a1e0411de709db8db675aaa999d5d18737988
yuvalbl <yuval.bar.levi@gmail.com>
2021-11-21 12:15:18 +0200

    10 using hooks on header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
-import React, { SyntheticEvent } from 'react';
+import React, { ChangeEvent, useState } from 'react';
+import { useFormInput } from '../hooks/useFormInput';
 import { ViewFilterMode } from '../models/ViewFilterMode';
 import './Header.css';
 
@@ -7,78 +8,53 @@ interface IProps {
 	onViewFilterChange: (newValue: ViewFilterMode) => void;
 }
 
-interface IState {
-	value: string;
-	viewFilterValue: ViewFilterMode;
-	buttonEnabled: boolean;
-}
 
-class Header extends React.Component<IProps, IState> {
-	constructor(props: IProps) {
-		super(props);
-		this.state = {
-			value: '',
-			viewFilterValue: ViewFilterMode.All,
-			buttonEnabled: false,
-		};
+const Header: React.FC<IProps> = ({ onAdd, onViewFilterChange }) => {
+	const [buttonEnabled, setButtonEnabled] = useState(false);
+	const [viewFilterValue, setViewFilterValue] = useState(ViewFilterMode.All);
+	const updateButtonByInput = (newValue: string) => setButtonEnabled(newValue.length > 0)
+	const newItem = useFormInput('', updateButtonByInput);
+
+	const handleViewChange =(event: ChangeEvent<HTMLSelectElement>) => {
+		const value = parseInt(event.target.value);
+		setViewFilterValue(value);
+		onViewFilterChange(value);
 	}
 
-	clear() {
-		this.setState({
-			value: '',
-			buttonEnabled: false,
-		});
+	const clear = () => {
+		setButtonEnabled(false);
+		newItem.onChange('');
 	}
-	handleViewChange = (event: SyntheticEvent) => {
-		const value = parseInt((event.target as HTMLSelectElement).value);
-		this.setState({
-			viewFilterValue: value,
-		});
-		this.props.onViewFilterChange(value);
-	};
 
-	handleInputChange = (event: SyntheticEvent) => {
-		const value = (event.target as HTMLInputElement).value;
-		this.setState({
-			value,
-			buttonEnabled: value.length > 0,
-		});
-	};
-
-	handleClick = () => {
-		if (this.state.buttonEnabled) {
-			this.props.onAdd(this.state.value);
-			this.clear();
+	const onAddClicked = () => {
+		if (buttonEnabled) {
+			onAdd(newItem.value);
+			clear();
 		}
 	};
 
-	render(): React.ReactElement {
-		return (
-			<header className="header">
-				<h1>My Todo list</h1>
-				<div className="header-new-item">
-					<input value={this.state.value} onChange={this.handleInputChange} />
-					<button
-						onClick={this.handleClick}
-						disabled={!this.state.buttonEnabled}
-					>
-						Add
-					</button>
-				</div>
-				<div>
-					<label htmlFor="view-selector">View</label>
-					<select
-						id="view-selector"
-						value={this.state.viewFilterValue}
-						onChange={this.handleViewChange}
-					>
-						<option value={ViewFilterMode.All}>All</option>
-						<option value={ViewFilterMode.Completed}>Completed</option>
-					</select>
-				</div>
-			</header>
-		);
-	}
-}
+	return (
+		<header className="header">
+			<h1>My Todo list</h1>
+			<div className="header-new-item">
+				<input {...newItem} />
+				<button onClick={onAddClicked} disabled={!buttonEnabled}>
+					Add
+				</button>
+			</div>
+			<div>
+				<label htmlFor="view-selector">View</label>
+				<select
+					id="view-selector"
+					value={viewFilterValue}
+					onChange={handleViewChange}
+				>
+					<option value={ViewFilterMode.All}>All</option>
+					<option value={ViewFilterMode.Completed}>Completed</option>
+				</select>
+			</div>
+		</header>
+	);
+};
 
 export default Header;
